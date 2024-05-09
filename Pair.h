0b6c8a6c7fc6690b1b23e25a966bffd83c32bfc8
David40404 <88509902+David40404@users.noreply.github.com>
2024-05-09 22:34:43 +0300

    Create Pair.h

---
+++ Pair.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <ostream>
+
+namespace Pair {
+	template <typename _Ty1, typename _Ty2>
+	class Pair {
+		_Ty1 m_first;
+		_Ty2 m_second;
+	public:
+		Pair() {
+		}
+
+		Pair(_Ty1 m_first, _Ty2 m_second) : m_first(m_first), m_second(m_second) {
+		}
+
+		_Ty1& first() {
+			return m_first;
+		}
+
+		_Ty2& second() {
+			return m_second;
+		}
+
+		Pair <_Ty2, _Ty1> getSwapped() {
+			return Pair <_Ty2, _Ty1>(m_second, m_first);
+		}
+
+		Pair <_Ty1, _Ty2>& operator=(const Pair <_Ty1, _Ty2>& other) {
+			if (this != &other) {
+				this->m_first = other.m_first;
+				this->m_second = other.m_second;
+			}
+			return *this;
+		}
+
+		bool operator==(const Pair <_Ty1, _Ty2>& other) const {
+			return this->m_first == other.m_first and this->m_second == other.m_second;
+		}
+
+		friend std::ostream& operator<<(std::ostream& out, Pair <_Ty1, _Ty2>& _pair) {
+			out << _pair.first() << " " << _pair.second();
+			return out;
+		}
+	};
+}
